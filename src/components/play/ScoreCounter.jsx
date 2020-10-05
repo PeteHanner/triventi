7f3e20c36727b31d8831028e839f55aa41840a15
Pete Hanner <petehanner@gmail.com>
2020-10-04 20:25:00 -0400

    Style score bug

--- src/components/play/ScoreCounter.jsx
+++ src/components/play/ScoreCounter.jsx
@@ -1,13 +1,21 @@
 import React, { useContext } from 'react';
-import { Box } from '@chakra-ui/core';
+import { Box, Text } from '@chakra-ui/core';
 import { Context } from '../../store';
 
 const ScoreCounter = () => {
   const [state, dispatch] = useContext(Context);
 
   return (
-    <Box>
-      {state.score}
+    <Box
+      rounded="sm"
+      borderWidth="2px"
+      background="white"
+      padding=".5rem"
+      margin="1rem"
+    >
+      <Text fontWeight="bold">
+        {state.score}
+      </Text>
     </Box>
   );
 };
