1d265f7855e1a50baebc0fa4116b6aa79b26d844
Pete Hanner <petehanner@gmail.com>
2020-12-05 10:41:05 -0500

    Updates

---
+++ precache-manifest.fe3e128af3da21c83f764e557d18e422.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "037ee5880e3ad09501934f0a7e12f077",
+    "url": "/triventi/index.html"
+  },
+  {
+    "revision": "6cc3543ea662d63e8943",
+    "url": "/triventi/static/css/main.67cc3542.chunk.css"
+  },
+  {
+    "revision": "c905c2fa54c4bd12a653",
+    "url": "/triventi/static/js/2.407cae37.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/triventi/static/js/2.407cae37.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6cc3543ea662d63e8943",
+    "url": "/triventi/static/js/main.321f2214.chunk.js"
+  },
+  {
+    "revision": "fd5af91ebaa08a2deb0f",
+    "url": "/triventi/static/js/runtime-main.014cbab9.js"
+  }
+]);
--- src/utils/time.js
+++
@@ -1,7 +0,0 @@
-export const calculateMinutes = (tenthsOfSecond) => {
-  return Math.floor((tenthsOfSecond / 10) / 60);
-};
-
-export const calculateSecondsRemainder = (tenthsOfSecond) => {
-  return String(((tenthsOfSecond / 10) % 60).toFixed(1)).padStart(4, '0');
-};
